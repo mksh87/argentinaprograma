c66a0cf0aed61e7b45ba9d8cd62cfb28410de7c0
mksh87 <keshishian.marcos@gmail.com>
2023-05-29 07:57:56 -0300

    Formulario para equipo Fut5 - en proceso

---
+++ Practico0_bis/equipos.js
@@ -0,0 +1,10 @@
+let equipo=[]
+function agregarJugador(){
+    let jugador={nombre:"",posicion:"",edad:0};
+    nombre=prompt("Ingresar nombre del jugador");
+    posicion=prompt("Ingresar posición en el campo de juego");
+    edad=prompt("Indicar edad del jugador");
+    equipo.push(jugador);
+}
+
+// me quedé acá... al final me mandé a hacerlo con un formulario en HTML. Ver equipos.html
