63e7db560b1d7ead0827dd5bae12a1b3205530c0
mksh87 <keshishian.marcos@gmail.com>
2023-05-30 20:57:31 -0300

    Limpiar archivos innecesarios

--- Otros/boton_variable.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector('button');
-
-button.onclick = function() {
-  let name = prompt('¿Cuál es tu nombre?');
-  alert('¡Hola ' + name + ', encantado de verte!');
-}
