323daed810b3d6b084061d59de1b4e3ebaac16cd
mksh87 <keshishian.marcos@gmail.com>
2023-05-30 21:03:03 -0300

    .

--- Practico0_bis/equipos.js
+++ Practico0_bis/equipos.js
@@ -1,16 +1,3 @@
-/*
-let equipo=[]
-function agregarJugador(){
-    let jugador={nombre:"",posicion:"",edad:0};
-    nombre=prompt("Ingresar nombre del jugador");
-    posicion=prompt("Ingresar posición en el campo de juego");
-    edad=prompt("Indicar edad del jugador");
-    equipo.push(jugador);
-}
-
-// me quedé acá... al final me mandé a hacerlo con un formulario en HTML. Ver equipos.html
-*/
-
     //defino algunas variables que utilizan otras funciones
     var equipo=[];
     var selPlayer
